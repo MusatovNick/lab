67372859d414416c8ecc936ec438eb9a850dcd78
NickElfee <mysatovnick@gmail.com>
2021-07-06 12:42:38 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const firstNumber = parseInt(prompt('firstNumber'));
+const secondNumber = isNaN(firstNumber) || parseInt(prompt('secondNumber'));
+
+if(!isNaN(firstNumber) && !isNaN(secondNumber)){
+    console.log(`Ответ: ${firstNumber + secondNumber}, ${firstNumber / secondNumber}`)
+}
+else {
+    console.log('Некорректный ввод!');
+}
+
+
+
+// locale
+// origin (remote)
